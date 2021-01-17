f236a747213f06e602758f67286055cc585994d5
Modurotolu Olokode <durolawk@gmail.com>
2021-01-17 13:06:13 +0100

    send publish message to subscribe

--- publisher/publisher-router.js
+++ publisher/publisher-router.js
@@ -1,9 +1,12 @@
 const publisher_router = require('express').Router();
 
 publisher_router.post('/:topic', (req, res) => {
-  const postInfo = { ...req.body, topic: req.params.topic };
-  console.log(postInfo.topic, postInfo.msg);
-  axios.post(`http://localhost:5000/subscribe`)
+  const topic =  req.params.topic;
+  console.log(req.body);
+  axios.post(`http://localhost:5000/subscribe/foward`, {
+    topic,
+    'data': req.body
+  })
     .then(message => {
       res.status(200).json(message)
     })
