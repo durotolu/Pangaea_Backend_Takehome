5e6f44cc4d1492f6239d63276a08adf4daa5dccd
Modurotolu Olokode <durolawk@gmail.com>
2021-01-17 02:25:16 +0100

    added post to push to subscribe from publisher

--- publish/publish-router.js
+++ publish/publish-router.js
@@ -3,6 +3,15 @@ const router = require('express').Router();
 router.post('/:topic', (req, res) => {
   const postInfo = { ...req.body, topic: req.params.topic };
   console.log(postInfo.topic, postInfo.msg);
+  axios.post(`http://localhost:5000/subscribe`)
+    .then(message => {
+      res.status(200).json(message)
+    })
+    .catch(error => {
+      res.status(500).json({
+        'error': error.message
+      });
+    });
 });
 
 module.exports = router;
