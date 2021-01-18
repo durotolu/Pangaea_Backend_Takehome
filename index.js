d57ae214d6d57367960c89d6daa47e5606d65615
Modurotolu Olokode <durolawk@gmail.com>
2021-01-18 17:11:16 +0100

    added ample server to test forwarded topics

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const publisher_server = require('./api/publisher-server');
-const subscription_server = require('./api/subscription-server');
 require('dotenv').config();
 
-const publisher_port = process.env.PUBLISHER_PORT || 5000;
-const subscription_port = process.env.SUBCRIPTION_PORT || 6000;
+const publish_server = require('./api/publish-server');
+const subscribe_server = require('./api/subscribe-server');
+const sample_server = require('./sample/sample-server');
 
-publisher_server.listen(publisher_port, () => {
-  console.log(`\n=== Publish server listening on port ${publisher_port} ===\n`);
+const publish_port = process.env.PUBLISH_PORT || 5000;
+const subscribe_port = process.env.SUBSCRIBE_PORT || 6000;
+const sample_port = process.env.SAMPLE_PORT || 6000;
+
+publish_server.listen(publish_port, () => {
+  console.log(`\n=== Publish server listening on port ${publish_port} ===\n`);
 });
 
-subscription_server.listen(subscription_port, () => {
-  console.log(`\n=== Subscription server listening on port ${subscription_port} ===\n`);
+subscribe_server.listen(subscribe_port, () => {
+  console.log(`\n=== Subscribe server listening on port ${subscribe_port} ===\n`);
+});
+
+sample_server.listen(sample_port, () => {
+  console.log(`\n=== sample server listening on port ${sample_port} ===\n`);
 });
